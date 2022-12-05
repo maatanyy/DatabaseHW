c2e939978f3d780e13d03f94296e0704507319b7
maatanyy <maatanyy@naver.com>
2022-12-05 09:22:53 +0900

    Finish insert

--- final.py
+++ final.py
@@ -40,7 +40,7 @@ while 1:
 
     elif menu == 2:
         sql = "INSERT INTO member (memberNumber,memberName,memberId, memberPassword,memberAddress,memberPhone, memberRrn,memberEmail,centerTitle) VALUES (%s, %s, %s, %s, %s,%s,%s,%s,%s)"
-        print("등록할 멤버의 이름을 입력해주세요 : (멤버넘버, 이름, 아이디, 비밀번호, 주소, 전화번호, 주민등록번호, 이메일, 등록한 스포츠센터)")
+        print("등록할 멤버의 정보를 입력해주세요 : (멤버넘버, 이름, 아이디, 비밀번호, 주소, 전화번호, 주민등록번호, 이메일, 등록한 스포츠센터)")
         memberNumber = int(input())
         memberName = str(input())
         memberId = str(input())
@@ -53,27 +53,39 @@ while 1:
         cur.execute(sql, (memberNumber,memberName,memberId, memberPassword,memberAddress,memberPhone, memberRrn,memberEmail,centerTitle))
 
     elif menu == 3:
-        sql = "INSERT INTO teacher (teacherNumber, teacherName ) VALUES ()"
-        print("검색할 책의 이름을 입력해주세요: ")
-        searchBook = str(input())
-        cur.execute(sql, (searchBook,))
-        result = cur.fetchall()
-        for data in result:
-            print(data)
-
+        sql = "INSERT INTO teacher (teacherNumber, teacherName, teacherId, teacherPassword, teacherAddress, teacherPhone, teacherRrn, teacherEmail, centerTitle) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        print("등록할 강사의 정보를 입력해주세요 : (강사넘버, 이름, 아이디, 비밀번호, 주소, 전화번호, 주민등록번호, 이메일, 등록한 스포츠센터)")
+        teacherNumber = int(input())
+        teacherName = str(input())
+        teacherId = str(input())
+        teacherPassword = str(input())
+        teacherAddress = str(input())
+        teacherPhone = str(input())
+        teacherRrn = str(input())
+        teacherEmail = str(input())
+        centerTitle = str(input())
+        cur.execute(sql, (teacherNumber, teacherName, teacherId, teacherPassword, teacherAddress, teacherPhone, teacherRrn, teacherEmail, centerTitle))
+#lecture
     elif menu == 4:
-        sql = "SELECT * FROM Book"
-        cur.execute(sql)
-        result = cur.fetchall()
-        for row_data in result:
-            print(row_data)
-
+        sql = "INSERT INTO lecture (lectureNumber, lectureTotal, lectureLocation,  lectureTime, lecturePay, lectureteacherNumber) VALUES (%s, %s, %s, %s, %s, %s)"
+        print("등록할 강의 정보를 입력해주세요 : (강의번호, 강의정원수, 강의장소, 강의시간, 강의요금, 강의강사이름)")
+        lectureNumber = int(input())
+        lectureTotal = str(input())
+        lectureLocation = str(input())
+        lectureTime = int(input())
+        lecturePay = int(input())
+        lectureteacherNumber = int(input())
+        cur.execute(sql, (lectureNumber, lectureTotal, lectureLocation, lectureTime, lecturePay, lectureteacherNumber))
+#car
     elif menu == 5:
-        sql = "SELECT * FROM Book"
-        cur.execute(sql)
-        result = cur.fetchall()
-        for row_data in result:
-            print(row_data)
+        sql = "INSERT INTO car (carNumber, carType, carMember, carTeacher) VALUES (%s, %s, %s, %s)"
+        print("등록할 자동차 정보를 입력해주세요 : (자동차번호, 차종, 자동차소유 회원번호, 자동차소유 강사번호 )")
+        carNumber = int(input())
+        carType = str(input())
+        carMember = int(input())
+        carTeacher = int(input())
+        cur.execute(sql, (carNumber, carType, carMember, carTeacher))
+
 
     elif menu == 6:
         sql = "SELECT * FROM Book"
