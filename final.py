9dcc0c8e97250e0b52e7fbfd27ff08c8b17eb5e5
maatanyy <maatanyy@naver.com>
2022-12-05 14:31:20 +0900

    add search

--- final.py
+++ final.py
@@ -69,8 +69,9 @@ while 1:
         cur.execute(sql, (teacherNumber, teacherName, teacherId, teacherPassword, teacherAddress, teacherPhone, teacherRrn, teacherEmail, centerTitle))
 #lecture
     elif menu == 4:
-        sql = "INSERT INTO lecture (lectureNumber,lectureName, lectureTotal, lectureLocation,  lectureDay, startTime, endTime, lecturePay, lectureteacherNumber) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        print("등록할 강의 정보를 입력해주세요 : (강의번호, 강의이름, 강의정원수, 강의장소,강의요일, 강의시작시간,강의종료시간, 강의요금, 강의강사번호)")
+        sql = "INSERT INTO lecture (lectureCenter, lectureNumber,lectureName, lectureTotal, lectureLocation,  lectureDay, startTime, endTime, lecturePay, lectureteacherNumber) VALUES (%s,%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        print("등록할 강의 정보를 입력해주세요 : (강의센터, 강의번호, 강의이름, 강의정원수, 강의장소,강의요일, 강의시작시간,강의종료시간, 강의요금, 강의강사번호)")
+        lectureCenter = str(input())
         lectureNumber = int(input())
         lectureName = str(input())
         lectureTotal = str(input())
@@ -86,7 +87,7 @@ while 1:
         endTime = datetime.time(b1, b2, b3)
         lecturePay = int(input())
         lectureteacherNumber = int(input())
-        cur.execute(sql, (lectureNumber, lectureName, lectureTotal, lectureLocation, lectureDay, startTime, endTime, lecturePay, lectureteacherNumber))
+        cur.execute(sql, (lectureCenter, lectureNumber, lectureName, lectureTotal, lectureLocation, lectureDay, startTime, endTime, lecturePay, lectureteacherNumber))
 #car
     elif menu == 5:
         sql = "INSERT INTO car (carNumber, carType, carMember, carTeacher) VALUES (%s, %s, %s, %s)"
@@ -122,11 +123,13 @@ while 1:
         minsungdb.commit()
 
     elif menu == 9:
-        sql = "SELECT * FROM Book"
-        cur.execute(sql)
-        result = cur.fetchall()
-        for row_data in result:
-            print(row_data)
+        sql = "DELETE FROM lecture WHERE lectureteacherNumber =%s AND lectureNumber = %s"
+        print("삭제할 강사가 다니는 스포츠센터 이름과 강사 번호와 강의번호를 입력해주세요: ")
+        deleteCenter = str(input())
+        deleteTeacher = str(input())
+        deleteLecture = str(input())
+        cur.execute(sql, (deleteTeacher, deleteLecture,))
+        minsungdb.commit()
 
     elif menu == 10:
         sql = "SELECT * FROM Book"
@@ -136,32 +139,40 @@ while 1:
             print(row_data)
 
     elif menu == 11:
-        sql = "SELECT * FROM Book"
-        cur.execute(sql)
+        sql = "SELECT * FROM center WHERE title = %s"
+        print("정보를 원하는 센터 이름을 입력해주세요: ")
+        searchCenter = str(input())
+        cur.execute(sql, (searchCenter,))
         result = cur.fetchall()
-        for row_data in result:
-            print(row_data)
+        for data in result:
+            print(data)
 
     elif menu == 12:
-        sql = "SELECT * FROM Book"
-        cur.execute(sql)
+        sql = "SELECT * FROM member WHERE memberName = %s"
+        print("검색을 원하는 회원 이름을 입력해주세요: ")
+        searchMember = str(input())
+        cur.execute(sql, (searchMember,))
         result = cur.fetchall()
-        for row_data in result:
-            print(row_data)
+        for data in result:
+            print(data)
 
     elif menu == 13:
-        sql = "SELECT * FROM Book"
-        cur.execute(sql)
+        sql = "SELECT * FROM teacher WHERE teacherName = %s"
+        print("검색을 원하는 강사 이름을 입력해주세요: ")
+        searchTeacher = str(input())
+        cur.execute(sql, (searchTeacher,))
         result = cur.fetchall()
-        for row_data in result:
-            print(row_data)
+        for data in result:
+            print(data)
 
     elif menu == 14:
-        sql = "SELECT * FROM Book"
-        cur.execute(sql)
+        sql = "SELECT * FROM lecture WHERE lectureName = %s"
+        print("검색을 원하는 강의 이름을 입력해주세요: ")
+        lectureName = str(input())
+        cur.execute(sql, (lectureName,))
         result = cur.fetchall()
-        for row_data in result:
-            print(row_data)
+        for data in result:
+            print(data)
 
     elif menu == 15:
         sql = "SELECT * FROM Book"
